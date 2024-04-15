a541a9c435225dcc3833b8337de803cdf24963b5
VanGuardf <alisaadati18000@gmail.com>
2024-04-15 21:55:58 +0330

    Add todo state and update it with input value

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap CSS
 
+// const list = [
+//   {}
+// ]
+
 export default function App() {
+  const [todo, setToDo] = useState("");
+  return <UI todo={todo} setToDo={setToDo} />;
+}
+
+function UI({ todo, setToDo }) {
   return (
     <div className="container">
       <form onSubmit={HandleSubmit}>
-        <input type="text" />
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => setToDo(e.target.value)}
+        />
         <button>Submit</button>
       </form>
     </div>
@@ -15,6 +28,6 @@ export default function App() {
 }
 
 function HandleSubmit(e) {
-  const [list, setList] = useState("");
+  e.preventDefault();
   console.log(e);
 }
